6d9ad38effbc5bfa7d77bb29c4ef1373d2f070e2
drmixer <130939838+drmixer@users.noreply.github.com>
2025-06-09 01:30:32 -0700

    Fix useEffect import in LandingPage component

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import Hero from './Hero';
 import Features from './Features';
