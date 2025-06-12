973d212f1fefc9a655cdbb09f3a9c6b87aaa4799
drmixer <130939838+drmixer@users.noreply.github.com>
2025-06-11 22:25:19 -0700

    Fix ToolsGrid execution issues

--- src/components/ToolsGrid.tsx
+++ src/components/ToolsGrid.tsx
@@ -165,7 +165,6 @@ const ToolsGrid: React.FC<ToolsGridProps> = ({
   useEffect(() => {
     if (selectedTool) {
       setExpandedTool(selectedTool);
-      // Don't automatically run the tool when it's selected
     }
   }, [selectedTool]);
 
